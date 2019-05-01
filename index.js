acd3d0f950f2cac5fc5df0f7a6a40bb708745517
Piet Geursen <pietgeursen@gmail.com>
2019-05-01 14:55:18 +1200

    Handles re-init when rebuilding

--- index.js
+++ index.js
@@ -8,30 +8,36 @@ module.exports = {
   version: '1.0.0',
   manifest: require('./manifest.json'),
   init: function (ssb, config) {
-    var log = OffsetLog('/home/piet/butt.offset', {
-      flags: 'a+',
-      codec: {
-        decode: JSON.parse,
-        encode: JSON.stringify,
-        buffer: false,
-        type: 'ssb'
-      }
-    })
+    var log
+    var state
 
-    var state = Validate.initial()
-
-    log.since.once(val => {
-      log.get(val, function (err, latestMessage) {
-        if (err) throw new Error(err)
-        state.feeds[ssb.id] = {
-          id: latestMessage.key,
-          timestamp: latestMessage.value.timestamp,
-          sequence: latestMessage.value.sequence,
-          queue: []
+    function init () {
+      log = OffsetLog('/home/piet/butt.offset', {
+        flags: 'a+',
+        codec: {
+          decode: JSON.parse,
+          encode: JSON.stringify,
+          buffer: false,
+          type: 'ssb'
         }
       })
-    })
 
+      state = Validate.initial()
+
+      log.since.once(val => {
+        log.get(val, function (err, latestMessage) {
+          if (err) throw new Error(err)
+          state.feeds[ssb.id] = {
+            id: latestMessage.key,
+            timestamp: latestMessage.value.timestamp,
+            sequence: latestMessage.value.sequence,
+            queue: []
+          }
+        })
+      })
+    }
+
+    init()
     // CACHE!!?????
     ssb.publish.hook(function (fn, args) {
       publish.apply(this, args)
@@ -67,7 +73,11 @@ module.exports = {
             log.append([msg], cb)
             console.log('+')
           }),
-          pull.drain(null, cb)
+          pull.drain(null, function (err) {
+            if (err) return cb(err)
+            init()
+            cb()
+          })
         )
       }
     }
