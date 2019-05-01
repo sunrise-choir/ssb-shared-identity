bdf7575b55fdfad74d3c20aee0de815a12f12c64
Piet Geursen <pietgeursen@gmail.com>
2019-05-01 14:48:14 +1200

    Hoox publish, appends valid message to butt.offset.

--- index.js
+++ index.js
@@ -1,18 +1,58 @@
 const pull = require('pull-stream')
-var OffsetLog = require('flumelog-offset')
+const OffsetLog = require('flumelog-offset')
+const Validate = require('ssb-validate')
+const timestamp = require('monotonic-timestamp')
+
 module.exports = {
   name: 'shared-identity',
   version: '1.0.0',
   manifest: require('./manifest.json'),
   init: function (ssb, config) {
+    var log = OffsetLog('/home/piet/butt.offset', {
+      flags: 'a+',
+      codec: {
+        decode: JSON.parse,
+        encode: JSON.stringify,
+        buffer: false,
+        type: 'ssb'
+      }
+    })
+
+    var state = Validate.initial()
+
+    log.since.once(val => {
+      log.get(val, function (err, latestMessage) {
+        if (err) throw new Error(err)
+        state.feeds[ssb.id] = {
+          id: latestMessage.key,
+          timestamp: latestMessage.value.timestamp,
+          sequence: latestMessage.value.sequence,
+          queue: []
+        }
+      })
+    })
+
     // CACHE!!?????
+    ssb.publish.hook(function (fn, args) {
+      publish.apply(this, args)
+    })
+
+    function publish (content, cb) {
+      Validate.appendNew(state, null, config.keys, content, timestamp())
+      var message = state.queue.pop()
+      log.append(message, function (err, result) {
+        if (err) return cb(err)
+        cb(null, message)
+      })
+    }
+
     return {
       rebuild: function (opts, cb) {
         if (typeof opts === 'function' && !cb) {
           cb = opts
           opts = {}
         }
-        var log = OffsetLog('/Users/matt/butt.offset', {
+        var log = OffsetLog('/home/piet/butt.offset', {
           flags: 'w',
           codec: {
             decode: JSON.parse,
